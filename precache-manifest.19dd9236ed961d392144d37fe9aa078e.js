514d2043c92495be6714ba48234503b2bec6a34c
Resilient08 <58645288+Resilient08@users.noreply.github.com>
2020-07-10 15:54:28 -0400

    Updates

---
+++ precache-manifest.19dd9236ed961d392144d37fe9aa078e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa5d4ad2954ddc25414f993043703c8e",
+    "url": "/sba.javascript_3-page-website/index.html"
+  },
+  {
+    "revision": "b8c242a663d4a617c776",
+    "url": "/sba.javascript_3-page-website/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3890df1a39c0f8ad8437",
+    "url": "/sba.javascript_3-page-website/static/css/main.ea19ca9e.chunk.css"
+  },
+  {
+    "revision": "b8c242a663d4a617c776",
+    "url": "/sba.javascript_3-page-website/static/js/2.c7991b8a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sba.javascript_3-page-website/static/js/2.c7991b8a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3890df1a39c0f8ad8437",
+    "url": "/sba.javascript_3-page-website/static/js/main.082ff2fd.chunk.js"
+  },
+  {
+    "revision": "02261cd0ec1e83407636",
+    "url": "/sba.javascript_3-page-website/static/js/runtime-main.34d2a35b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/sba.javascript_3-page-website/static/media/SEGA.12a3db8b.woff"
+  }
+]);
